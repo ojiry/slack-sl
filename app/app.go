53561dde0c82399066fe3230c85072fc839ca7bb
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2019-10-28 19:30:38 +0900

    Change to use env for port

--- app/app.go
+++ app/app.go
@@ -1,12 +1,21 @@
 package app
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	scHandler "github.com/ojiry/slack-sl/app/slash_commands/handler"
 )
 
 func Run() error {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	http.HandleFunc("/slash_commands", scHandler.SlashCommandsHandler)
-	return http.ListenAndServe(":8080", nil)
+
+	return http.ListenAndServe(":"+port, nil)
 }
