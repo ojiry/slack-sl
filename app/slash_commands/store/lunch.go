495a3d28c75e63a6e3425ef3408f98546d6ae5c3
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2019-11-07 20:30:16 +0900

    Persist data using gorm

--- app/slash_commands/store/lunch.go
+++ app/slash_commands/store/lunch.go
@@ -4,16 +4,26 @@ import (
 	"fmt"
 
 	"github.com/ojiry/slack-sl/app/slash_commands"
+
+	"github.com/jinzhu/gorm"
 )
 
-type LunchStore struct{}
+type Lunch struct {
+	gorm.Model
+}
 
-func NewLunchStore() *LunchStore {
-	return &LunchStore{}
+type LunchStore struct {
+	db *gorm.DB
+}
+
+func NewLunchStore(db *gorm.DB) *LunchStore {
+	return &LunchStore{db: db}
 }
 
 func (s *LunchStore) Create(l slash_commands.Lunch) (*slash_commands.Lunch, error) {
-	r := &slash_commands.Lunch{}
-	fmt.Println("The lunch was created successfully.")
+	lunch := Lunch{}
+	s.db.Create(&lunch)
+	fmt.Printf("The lunch was created successfully. %v", lunch)
+	r := &slash_commands.Lunch{ID: lunch.ID}
 	return r, nil
 }
